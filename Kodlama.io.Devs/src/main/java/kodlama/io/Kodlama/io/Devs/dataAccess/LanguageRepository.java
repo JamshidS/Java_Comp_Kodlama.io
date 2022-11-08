111ef53174d2a9d8b113803a816059b368ea51c9
Jamshid Sadiqi <77556948+JamshidS@users.noreply.github.com>
2022-11-08 17:46:59 +0300

    Delete Kodlama.io.Devs/src/main/java/kodlama/io/Kodlama/io/Devs directory

--- Kodlama.io.Devs/src/main/java/kodlama/io/Kodlama/io/Devs/dataAccess/LanguageRepository.java
+++
@@ -1,7 +0,0 @@
-package kodlama.io.Kodlama.io.Devs.dataAccess;
-
-import kodlama.io.Kodlama.io.Devs.entities.concretes.Language;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LanguageRepository extends JpaRepository<Language,Integer> {
-}
